dc0e7a0e1a0f56bea23e1e3c02e15b790fe6ea1f
Raul Escobar <raul.escobar.akd@gmail.com>
2022-07-09 16:06:08 -0300

    repaso sabado

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,9 +4,12 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	
+	//ingreso de variables.
 	let numeroUno;
 	let numeroDos;
 	let resultado;
+	let mensaje;
 
 	numeroUno=document.getElementById("txtIdNumeroUno").value;
 	numeroUno=parseInt(numeroUno); // "parseInt" se utiliza para convertirlo en numero entero.
@@ -14,8 +17,34 @@ function sumar()
 	numeroDos=parseInt(numeroDos);
 	
 	resultado=numeroUno+numeroDos;
+
+	mensaje="la suma es "+ resultado;
 	
-	alert("la suma es " + resultado);
+	alert(mensaje);
 	//alert(numeroUno+numeroDos);
+	
+
+	/*
+	//CLASE DE REPASO: SABADO 9/7 EJERCICIO
+
+	let dolarOficial;
+	let dolarBlue;
+	let porcentaje;
+	let mensaje;
+
+
+	dolarOficial=document.getElementById("txtIdNumeroUno").value;
+	dolarOficial=parseInt(dolarOficial)
+
+	dolarBlue=document.getElementById("txtIdNumeroDos").value;
+	dolarBlue=parseInt(dolarBlue);
+
+	//mal
+	porcentaje=((dolarBlue-dolarOficial)*100)/100;
+
+	mensaje="La diferencia entre el dolar Oficial y el Blue es de "+ porcentaje+ " %";
+
+	alert(mensaje);
+	*/
 }
 
